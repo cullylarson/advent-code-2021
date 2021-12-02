f870eaab426bdd104018921b627e29b7e8eaedb3
Cully Larson <cully.larson@gmail.com>
2021-12-01 21:12:31 -0800

    Add day 2, part 2.

---
+++ day-02/part-2.js
@@ -0,0 +1,41 @@
+import {compose, reduce, report, filter, map, split, toInt} from '@cullylarson/f'
+import {then} from '@cullylarson/p'
+import {readFile} from '../lib.js'
+
+const lineToCommand = ([command, unitsStr]) => {
+  return {
+    command,
+    units: toInt(null, unitsStr),
+  }
+}
+
+const commandsToPosition = reduce((acc, x) => {
+  switch(x.command) {
+    case 'forward':
+      return {
+        ...acc,
+        horizontal: acc.horizontal + x.units,
+        depth: acc.depth + (acc.aim * x.units),
+      }
+    case 'up':
+      return {
+        ...acc,
+        aim: acc.aim - x.units,
+      }
+    case 'down':
+      return {
+        ...acc,
+        aim: acc.aim + x.units,
+      }
+  }
+}, {horizontal: 0, depth: 0, aim: 0})
+
+then(compose(
+  report,
+  x => x.horizontal * x.depth,
+  commandsToPosition,
+  map(lineToCommand),
+  map(split(' ')),
+  filter(Boolean),
+  split('\n'),
+), readFile('input.txt', {encoding: 'utf8'}))
