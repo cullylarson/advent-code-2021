44d3aff8e04435a5d534888a1ce2321dde687147
Cully Larson <cully.larson@gmail.com>
2021-12-05 18:08:58 -0800

    Add day 5, part 2.

--- day-05/lib.js
+++ day-05/lib.js
@@ -2,9 +2,9 @@ import {then} from '@cullylarson/p'
 import {compose, filter, trim, map, split, toInt, last, curry} from '@cullylarson/f'
 import {readFile} from '../lib.js'
 
-export const flat = xs => xs.flat()
+const flat = xs => xs.flat()
 
-export const sort = xs => [...xs].sort((a, b) => a - b)
+const sort = xs => [...xs].sort((a, b) => a - b)
 
 const getMostDigts = compose(
   x => ('' + x).length,
@@ -34,6 +34,83 @@ export const printBoard = board => {
   }
 
   return board
+}
+
+const length = xs => xs.length
+
+export const getNumOverlaps = curry((minOverlaps, board) => {
+  return compose(
+    length,
+    filter(x => x >= minOverlaps),
+    flat,
+    flat,
+  )(board)
+})
+
+const getBoardSize = compose(
+  last,
+  sort,
+  flat,
+  flat,
+)
+
+export const createBoard = (lines) => {
+  const size = getBoardSize(lines)
+
+  const board = []
+  for(let y = 0; y <= size; y++) {
+    const row = Array(size + 1).fill(0)
+    board.push(row)
+  }
+
+  return {board, lines}
+}
+
+const markPoint = (board, x, y) => {
+  board[y][x]++
+  return board
+}
+
+const isInteger = x => Math.floor(x) === x
+
+const markLine = (board, line) => {
+  const [x0, y0] = line[0]
+  const [x1, y1] = line[1]
+
+  // straight line, vertical
+  if(x0 === x1) {
+    const x = x0
+    for(let y = Math.min(y0, y1); y <= Math.max(y0, y1); y++) {
+      board = markPoint(board, x, y)
+    }
+  }
+  // straight line, horizontal
+  else if(y0 === y1) {
+    const y = y0
+    for(let x = Math.min(x0, x1); x <= Math.max(x0, x1); x++) {
+      board = markPoint(board, x, y)
+    }
+  }
+  else {
+    const run = x1 - x0
+    const rise = y1 - y0
+    const m = run === 0 ? 0 : rise / run
+    const b = y0 - (m * x0)
+
+    for(let x = Math.min(x0, x1); x <= Math.max(x0, x1); x++) {
+      const y = (m * x) + b
+
+      if(isInteger(y)) {
+        board = markPoint(board, x, y)
+      }
+    }
+  }
+
+  return board
+}
+
+export const markLines = ({board, lines}) => {
+  return lines.reduce(markLine, board)
 }
 
 export const readInput = fileName => then(compose(
--- day-05/part-2.js
+++ day-05/part-2.js
@@ -1,15 +1,12 @@
 import {then} from '@cullylarson/p'
-import {compose, filter, report} from '@cullylarson/f'
+import {compose, report} from '@cullylarson/f'
 import {readInput, markLines, createBoard, getNumOverlaps} from './lib.js'
 
 const MIN_OVERLAPS = 2
-
-const onlyStraightLines = filter(([[x0, y0], [x1, y1]]) => x0 === x1 || y0 === y1)
 
 then(compose(
   report,
   getNumOverlaps(MIN_OVERLAPS),
   markLines,
   createBoard,
-  onlyStraightLines,
 ), readInput('input.txt'))
